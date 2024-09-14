2a60927eff2f0a4bcd68e091bb12df7170d55f6a
Eduardo H. M. Cruz <eduardohmdacruz@gmail.com>
2024-09-14 16:23:01 -0300

    improved sleep.asm

--- asm-example/sleep.asm
+++ asm-example/sleep.asm
@@ -9,6 +9,23 @@ loop:
 	mov r0, 1             # 1 is print string service
 	syscall
 
+	mov r1, 2
+	mov r0, 6             # 6 is sleep service
+	syscall
+
+	mov r1, wake_up_str
+	mov r0, 1             # 1 is print string service
+	syscall
+
+	mov r0, 7             # 7 is a service that returns time in r1
+	syscall
+
+	mov r0, 3             # 3 is integer print service
+	syscall
+
+	mov r0, 2             # 2 is newline print service
+	syscall
+
 	jump loop
 
 end:
